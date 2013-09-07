bdb30e2fb5da84a70672b86a14586959a2221e03
Matt Westcott <matt@west.co.tt>
2013-09-07 19:15:37 +0100

    make water molecule a bit less throbby.

--- democode/molecule.py
+++ democode/molecule.py
@@ -2,7 +2,7 @@ from democode.antialias import Antialiaser
 from democode import draw
 import math
 
-SEQ = [6,4,2,6,4,2,6,5,4,3,2,2,2,2,2,2]
+SEQ = [5,3,2,5,3,2,5,4,3,3,2,2,2,2,2,2]
 
 class MoleculeScene(object):
 	def __init__(self, lp):
@@ -27,32 +27,24 @@ class MoleculeScene(object):
 		x2 = 9
 		y2 = 4
 
+
+		draw.line(self.aa.screen, x0, y0, x2, y2, (0, 1.3))
+		draw.line(self.aa.screen, x1, y1, x2, y2, (0, 1.3))
+
 		section = (beat + 4) % 64
 		if section < 16:
-			draw.line(self.aa.screen, x0, y0, x2, y2, (0, 1.3))
-			draw.line(self.aa.screen, x1, y1, x2, y2, (0, 1.3))
-
 			draw.disc(self.aa.screen, x0, y0, big_disc_size, (1, 1))
 			draw.disc(self.aa.screen, x1, y1, small_disc_dize, (1, 1))
 			draw.disc(self.aa.screen, x2, y2, small_disc_dize, (1, 0))
 		elif section < 32:
-			draw.line(self.aa.screen, x0, y0, x2, y2, (0, 1.3))
-			draw.line(self.aa.screen, x1, y1, x2, y2, (0, 1.3))
-
 			draw.disc(self.aa.screen, x0, y0, small_disc_dize, (1, 1))
 			draw.disc(self.aa.screen, x1, y1, big_disc_size, (1, 1))
 			draw.disc(self.aa.screen, x2, y2, small_disc_dize, (1, 0))
 		elif section < 48:
-			draw.line(self.aa.screen, x0, y0, x2, y2, (0, 1.3))
-			draw.line(self.aa.screen, x1, y1, x2, y2, (0, 1.3))
-
 			draw.disc(self.aa.screen, x0, y0, small_disc_dize, (1, 1))
 			draw.disc(self.aa.screen, x1, y1, small_disc_dize, (1, 1))
 			draw.disc(self.aa.screen, x2, y2, big_disc_size, (1, 0))
 		else:
-			draw.line(self.aa.screen, x0, y0, x2, y2, (0, 1.3))
-			draw.line(self.aa.screen, x1, y1, x2, y2, (0, 1.3))
-
 			draw.disc(self.aa.screen, x0, y0, big_disc_size, (1, 1))
 			draw.disc(self.aa.screen, x1, y1, big_disc_size, (1, 1))
 			draw.disc(self.aa.screen, x2, y2, small_disc_dize, (1, 0))
